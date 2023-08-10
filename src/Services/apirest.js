a9f0fb3556e5472d04dd8846757e2c86e465542b
DanielDDsam <salaodxgotico91@gmail.com>
2023-08-10 12:00:50 -0400

    administrar, ultimo commit

--- src/Services/apirest.js
+++ src/Services/apirest.js
@@ -1,2 +1,2 @@
-//export const Apiurl = "https://prepaes-api.onrender.com/";
-export const Apiurl = "http://127.0.0.1:8000/";
+export const Apiurl = "https://prepaes-api.onrender.com/";
+//export const Apiurl = "http://127.0.0.1:8000/";
